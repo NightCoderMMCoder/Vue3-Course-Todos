a947fe393aa58e03919bbe80578a18a1da4c2146
aungmyat12 <aungmyatoo2580@gmail.com>
2021-05-21 00:54:42 +0630

    16-RealTimePart2

--- src/components/Todos/TodosList.vue
+++ src/components/Todos/TodosList.vue
@@ -27,15 +27,38 @@ export default {
     // });
     // todos.value = results;
     collectionRef.onSnapshot((snapshot) => {
-      console.log(snapshot);
-      let results = [];
-      snapshot.docs.forEach((doc) => {
-        results.push({
-          ...doc.data(),
-          id: doc.id,
-        });
+      // console.log(snapshot);
+      // let results = [];
+      // snapshot.docs.forEach((doc) => {
+      //   results.push({
+      //     ...doc.data(),
+      //     id: doc.id,
+      //   });
+      // });
+      // todos.value = results;
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          todos.value.push({
+            ...change.doc.data(),
+            id: change.doc.id,
+          });
+        }
+        if (change.type === "modified") {
+          const idx = todos.value.findIndex(
+            (todo) => todo.id === change.doc.id
+          );
+          Object.assign(todos.value[idx], {
+            ...change.doc.data(),
+            id: change.doc.id,
+          });
+        }
+        if (change.type === "removed") {
+          const idx = todos.value.findIndex(
+            (todo) => todo.id === change.doc.id
+          );
+          todos.value.splice(idx, 1);
+        }
       });
-      todos.value = results;
     });
 
     return { todos };
