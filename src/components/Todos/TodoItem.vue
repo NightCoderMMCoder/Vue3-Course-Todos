cbd79916dcee3b1f9c81e36427175d8333eacf7d
aungmyat12 <aungmyatoo2580@gmail.com>
2021-05-20 23:40:49 +0630

    11-AddTodo

---
+++ src/components/Todos/TodoItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="form-check">
+    <input
+      class="form-check-input"
+      type="checkbox"
+      value=""
+      id="flexCheckDefault"
+      :checked="todo.completed"
+    />
+    <label class="form-check-label" for="flexCheckDefault">
+      <p class="mb-0">
+        <span :class="{ completed: todo.completed }">{{ todo.task }}</span>
+        <br />
+        <small v-if="todo.dueDate">Overdue {{ todo.dueDate }} </small>
+      </p>
+      <span>
+        <i class="fas fa-pencil-alt"></i>
+        <i class="fas fa-trash"></i>
+        <i class="far fa-star" :class="{ important: todo.important }"></i>
+      </span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { todo: Object },
+};
+</script>
+
+<style scoped>
+.form-check {
+  border-bottom: 1px solid #bbbbbb;
+  padding: 20px;
+}
+.form-check-label {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.completed {
+  text-decoration: line-through;
+  opacity: 0.7;
+  transition: 0.3s linear;
+}
+.fa-calendar {
+  color: var(--primary);
+}
+.fa-star {
+  color: rgb(160, 160, 160);
+  cursor: pointer;
+}
+.important {
+  font-weight: bold;
+  color: var(--primary);
+}
+.overDue {
+  color: red;
+}
+.overDue .fas {
+  color: red;
+}
+.fa-trash {
+  margin: 0 20px;
+  color: #fa1e0e;
+  cursor: pointer;
+}
+.fa-pencil-alt {
+  color: #ffe227;
+  cursor: pointer;
+}
+</style>
--- src/views/Todos/AddTodo.vue
+++ src/views/Todos/AddTodo.vue
@@ -4,14 +4,14 @@
       Add Todo
     </div>
     <div class="card-body">
-      <form>
+      <form @submit.prevent="handleSubmit">
         <div class="mb-2">
           <label for="task" class="form-label">Task</label>
-          <input type="text" id="task" class="form-control" />
+          <input type="text" id="task" class="form-control" v-model="task" />
         </div>
         <div class="mb-3">
           <label for="date" class="form-label">Date</label>
-          <input type="date" id="date" class="form-control" />
+          <input type="date" id="date" class="form-control" v-model="dueDate" />
         </div>
         <button class="btn btn-primary">Add Todo</button>
       </form>
@@ -20,7 +20,27 @@
 </template>
 
 <script>
-export default {};
+import { reactive, toRefs } from "vue";
+import db from "../../firebase/init";
+export default {
+  setup() {
+    const todos = reactive({
+      task: "",
+      dueDate: "",
+    });
+
+    const handleSubmit = async () => {
+      const collectionRef = db.collection("todos");
+      const doc = await collectionRef.add({
+        ...todos,
+        createdAt: Date.now(),
+      });
+      console.log(doc);
+    };
+
+    return { ...toRefs(todos), handleSubmit };
+  },
+};
 </script>
 
 <style></style>
